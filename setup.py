8ddfe46b40f20fc2471478dadec5ef23eca14faf
Bruno Mlodozeniec <bkmlodozeniec@gmail.com>
2020-04-27 20:25:11 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='torchpilco',
+      version='0.1',
+      license='MIT',
+      description='Pytorch implementation of Deep PILCO',
+      author='Bruno Kacper Mlodozeniec',
+      author_email='bkm28@cam.ac.uk',
+      packages=['torchpilco'],
+      zip_safe=False)
